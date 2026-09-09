797919c141d6a8370a49ba342cf2e7f110634247
twotheg <twotheg@gmail.com>
2026-09-09 13:26:26 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
     // 타입스크립트 문법 오류가 있어도 배포를 강제로 진행합니다.
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // ESLint 경고/오류가 있어도 배포를 강제로 진행합니다.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
